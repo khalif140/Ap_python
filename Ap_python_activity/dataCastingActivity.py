f548bbc18ad71b620becb3a08e8bbcd71852faea
khalif140 <144815806+khalif140@users.noreply.github.com>
2023-09-21 13:55:16 +0000

    done

--- Ap_python_activity/dataCastingActivity.py
+++ Ap_python_activity/dataCastingActivity.py
@@ -13,7 +13,7 @@ num_two= 23.45
 print(int(num_one + num_two))
 
 # modify the following code below to
-name_of_pet = "Spuny"
+type(name_of_pet = "Spuny")
 age_of_pet = 10
 
 print('this is my dog '+ name_of_pet + '. He is ' + age_of_pet+ ' years old.')
