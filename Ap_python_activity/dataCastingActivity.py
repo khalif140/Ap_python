b4949b7a5dcfdc6f8d3bbce074ade3ae2b8a1ce6
khalif140 <144815806+khalif140@users.noreply.github.com>
2023-09-22 14:28:23 +0000

    change

--- Ap_python_activity/dataCastingActivity.py
+++ Ap_python_activity/dataCastingActivity.py
@@ -1,3 +1,7 @@
+# type() function will return the data type of the value.
+cash = 100
+print(type(cash))
+
 # convert the following datatypes into floats.
 print(float(12))
 print(float(100))
@@ -13,7 +17,7 @@ num_two= 23.45
 print(int(num_one + num_two))
 
 # modify the following code below to
-type(name_of_pet = "Spuny")
-age_of_pet = 10
+name_of_pet = "Spuny"
+age_of_pet = str(10)
 
 print('this is my dog '+ name_of_pet + '. He is ' + age_of_pet+ ' years old.')
