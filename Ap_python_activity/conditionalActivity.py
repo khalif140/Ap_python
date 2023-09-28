8dfb50c3cf3e2cce72718c6e43553df64e1ca3e3
khalif140 <144815806+khalif140@users.noreply.github.com>
2023-09-28 14:49:43 +0000

    info

--- Ap_python_activity/conditionalActivity.py
+++ Ap_python_activity/conditionalActivity.py
@@ -13,6 +13,13 @@
 
 # 3. write a simple conditional state using a comparision operator(greater than, less than, etc. )
 
+hearts = 4
+clubs = 10
+if b > a:
+    print('B is greater than A')
+else:
+    print('B is not greater than A')
+
 # 3. Write a conditional statement for food in the refridgerator.
 # your conditional statement should be wrapped in a function that takes one (1)
 # parameter. The data type for this parameter should be true or false. 
@@ -22,8 +29,25 @@
 # When you call your function there should be an argument that accepts 
 # a boolean. 
 
+def foodInFridge(food):
+
+    if food == True:
+        print('Time to cook')
+    else:
+        print('Time to shop')
+
+foodInFridge(True)
 # 4. Create a function that checks the inventory of cereal for a store. 
 # your function should parameter should accept an integer. In your function
 # use a conditional statement to determine if you need to order more cereal.
 # If there is more than 10 boxes, print "inventory full", else if there are less than 
 # 10 boxes print "we need to order more cereal".
+
+def check_cereal_inventory(quantity):
+    if quantity > 10:
+        print('inventory is full')
+    else:
+        print('order more cereal')
+
+print('________________')
+check_cereal_inventory(8)
