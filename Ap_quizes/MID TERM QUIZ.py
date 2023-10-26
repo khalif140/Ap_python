e41e9c47d18ddf19923e4d21599f7512602db06e
khalif140 <144815806+khalif140@users.noreply.github.com>
2023-10-26 15:23:00 +0000

    failed

---
+++ Ap_quizes/MID TERM QUIZ.py
@@ -0,0 +1,148 @@
+# MID TERM QUIZ 
+
+
+# RULES
+
+# I. You may use google/ W3 schools to assist you with this exam
+
+# II. You may use notes in your reposiory
+
+# III. You may not use any additional computing devices 
+# (only one computer should be out).
+
+# IV. You may not use your phone during the quiz under any circumstances.
+# If it is an emergency please request to be excused from the class. 
+
+# V. You may not have music or videos playing during the quiz
+# under any circumstances. 
+
+# VI. You may not use any LLM / AI applications under any circumstances. 
+# Any one caught using  these applications will automatically fail the quiz.
+
+# _____________________________________________________________________
+
+
+# Take your time, read the questions thoroughly, look for context clues
+
+# GOOD LUCK
+
+# _____________________________________________________________________
+
+
+
+# 1. Describe what a computer program is and what does it do. 
+
+# A computer program is insturctions that are read by a computer to perform a specific action 
+# within the code that you but you have clear on what you want it to do
+
+
+# 2. Describe what complexitity and abstraction are, then provide an example
+# of complexity in the real world and how we apply abstraction to that thing (you example must be original and cannot be an example
+# used in our lecture slides ie car, grocery store, etc.). 
+  
+# A complexitity is how complicated it is for a developer to understand.
+# abstraction is suppose to hide all but the necessary data of an object in order
+# to reduce complexity in the program
+
+#we would probably  use  for like traffic like 
+
+# 3. What is Syntax in Python and why is it important to write proper syntax?
+
+# syntax is the control for the struture of the code  for like symbol or punctuation
+# with out that not many would understand
+
+# 4. What is a function, and why do we wrap our code inside of functions?
+
+# A fuction is the program instruction to whatever code you put just have to be specific
+# the reason why we wrap our code is so when we need it we just have to call it with its name
+
+# 5. Name and describe the three (3) naming conventions for variables in python? Then provide three (3) name rules for Python
+# variables. 
+
+# Snakecase separate words are separated by underscores
+# Camelcase the first letter is lowercase and each subsequent word in a name begins with an 
+# uppercase letter 
+# Pascalcase each word in a variable name begins with an uppercase letter
+
+# 1. you can not start a variable off with a number
+# 2. variable names are case-sensitive the varible should no matter if its the same word
+# 3. a variable name must start with a letter or the underscore character
+
+
+# 6. What will be the output of the print functons when this code is ran, explain why
+name = 'Bill'
+student = name
+name = 'Walter'
+student= 'Richard'
+
+print(name)
+print(student)
+
+# the output would print just  walter and richard since it goes from the top to the bottom its 
+# answer will change after see bill
+
+# 7. Describe the difference between each of the following assignment operators:
+# = 
+# ==
+# !=
+ 
+# 8. What type of data can be stored in a python list?
+#list
+#str
+#int
+#bool
+# 9. Create a function that will take in a word provided by a user and then output that word back to the user but in reverse. 
+
+
+# 10. Create three (3) seperate functions that will do addition, subtraction, and multiplication respectively. 
+# each of these functions should take two (2) arguments. When the user passes these arguments into the function, they will be
+# calculated together and return the output in your terminal. 
+
+# 11. What is the difference between a function argument and a function parameter. 
+
+# 12. You have been hired by a software company and your first assignment is to develop a password authenticator program. 
+# the goal of your program is to check a user's password and make sure it meets the company's password requirements. 
+# the company has provided you with the following requirements for the passcode program:
+# a. the passcode must NOT contain any numbers
+# b. the passcode must NOT contain any capital letters
+# c. the passcode can NOT be any longer than five (5) characters
+# d. the passcode can NOT shorter the three (3) characters
+# e. the user should be able to enter their password and if it meets the requirements, should print a message saying that 
+# their password was created successfully, and if it was not, should send back a message with one of the following issues. 
+
+# 13. When you run your code and see typeError in your terminal, what does that typically mean and how would you try to solve
+# the issue?
+
+# 14. When you run your code and see indentError in your terminal, what does that typically mean and how would you try to solve 
+# the issue?
+
+# 15. Create a while loop that check a user's password. If they enter the password correct, they will get a message saying 
+# that the password was entered successfully. If they enter the password incorrectly, it will tell the user to try again. 
+# The user should only have three (3) attempts to get the password correct. If they enter the password incorrect on the fourth 
+# attempt, a message should appear telling them that have been locked out and need to talk to the administrator. 
+
+# 16. Which item is at index 5
+giftShopping=['xbox','sneakers','necklace','clothing','laptop','gift card']
+
+# 17. Create a for loop that will print ONLY the even numbers within the range of the variable provided below.
+# HINT: You will need to use the range() function. 
+upToNumber = 30
+
+# 18. Create a function that uses a conditional statement that checks if a person qualifies for a raise on their salary. 
+# The user should be able to enter their name, their salary (how much money they make in an entire year), and how long they have
+# worked at the company. If the user has been working at the company longer than four (4) years, they will get a 15% raise. 
+# Your program should be able to calculate what their salary is with the 15% increase. If the user qualifies, your program
+# should return the a message congratulating the user by name, and telling them what their new salary is with the 15%
+# increase (this must be the actual number). If they do not qualify inform the user that unfortunately they do not qualify. 
+
+# 19. Create a function that checks which value is greater than the other. Your function should take two (2) integer parameters. 
+# and should evaluate which number is larger. Your function should then print the largest number. 
+
+# 20. Create a while loop function that will add gift items to a list. Your function should ask the user to enter an item name. 
+# The item name should then be added to a list variable called gift_bag. Your gift bag should have a limit of six (6) items. 
+# Once your gift bag hits its limit of six (6) items your program should print a message saying 
+# that the gift bag is full and print the list of items in the gift bag.   
+ 
+
+
+ 
